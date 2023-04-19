6cf131aca6b775d95a9c61f3f7e270d9777bf553
David Lee <david.lee@datadoghq.com>
2023-04-19 16:42:18 -0700

    clarify comment

--- ddtrace/internal/serverless/__init__.py
+++ ddtrace/internal/serverless/__init__.py
@@ -22,8 +22,8 @@ def has_aws_lambda_agent_extension():
 def in_gcp_function():
     # type: () -> bool
     """Returns whether the environment is a GCP Function.
-    This is accomplished by checking if the FUNCTION_NAME or K_SERVICE
-    environment variables are defined.
+    This is accomplished by checking for the presence of one of two pairs of environment variables,
+    with one pair being set by deprecated GCP Function runtimes, and the other set by newer runtimes.
     """
     is_deprecated_gcp_function = bool(environ.get("FUNCTION_NAME", False)) and bool(environ.get("GCP_PROJECT", False))
     is_newer_gcp_function = bool(environ.get("K_SERVICE", False)) and bool(environ.get("FUNCTION_TARGET", False))
